fd1d235031de30f8ba0ed4819348750f8e3bb255
EvanCoyle <evan.g.coyle@gmail.com>
2025-12-28 10:20:27 -0500

    Seed default user in data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,4 +1,11 @@
+DELETE FROM users_session_activity;
+DELETE FROM users_session;
 DELETE FROM navigation;
+DELETE FROM users;
+
+INSERT INTO users (user_sid, email, password_hash, name, avatar, status) OVERRIDING SYSTEM VALUE
+VALUES
+    (1, 'evan.g.coyle@gmail.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Evan Coyle', 'images/avatars/male-04.jpg', 'online');
 
 INSERT INTO navigation (navigation_sid, user_sid, parent_navigation_sid, navigation_key, item_id, title, subtitle, type, icon, link, tooltip, badge_title, badge_classes, active, disabled, exact_match, sort_order) OVERRIDING SYSTEM VALUE
 VALUES
